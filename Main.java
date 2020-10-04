9be02cb5a2810c009d07b68b6abd238f95b39b31
Gormi3815 <gormi3815@wrdsb.ca>
2020-10-04 18:59:55 +0000

    will tell you the percentage of a letter grade

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -10,6 +11,25 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter a letter grade");
+    String grade = input.nextLine();
+    switch(grade){
+      case "A":
+      System.out.println("This is between 80% and 100%");
+      break;
+      case "B":
+      System.out.println("This is between 70% and 79%");
+      break;
+      case "C":
+      System.out.println("This is between 60% and 69%");
+      break;
+      case "D":
+      System.out.println("This is between 50% and 59%");
+      break;
+      case "F":
+      System.out.println("This is between 0% and 49%");
+      break;
+    }
   }
 }
